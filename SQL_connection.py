754631477c3dbcd8651676459343438ab7b53633
Kanishk-mittal <kanishkmittal2005@gmail.com>
2023-04-12 10:20:08 +0530

    completed till a function to add tenant details to database

--- SQL_connection.py
+++ SQL_connection.py
@@ -1,15 +1,31 @@
 import mysql.connector as msc
+import pandas as pd
+con=msc.connect(host="localhost",user="root",password="1234")
+cur=con.cursor()
+
+def add_room(room_name,rent,currently_occupied,internet_provided,last_electricity_unit,agreement_ID,agreement_Date):
+    cur.execute(f"insert into Room_Details(Room_name,rent,currently_occupied,internet_provided,last_electricity_unit,agreement_ID,agreement_Date ) values('{room_name}',{rent},'{currently_occupied}','{internet_provided}',{last_electricity_unit},'{agreement_ID}','{agreement_Date}')")
+
 def initialise():
-    con=msc.connect(host="localhost",user="root",password="1234")
-    cur=con.cursor()
     try:
         cur.execute("Use Rent_details;")
     except:
         cur.execute("Create Database Rent_details;")
         cur.execute("Use Rent_details;")
-        cur.execute("Create table Tenant_Details(Tenant_ID int(5),Name varchar(25),address varchar(50),room_alloted varchar(10),last_balance int(10),extra_charge int(10),extra_charge_comment varchar(50),rent_paid_till date,left_room_on date);")
-        cur.execute("Create table Room_Details(room_ID int(5),rent int(6),currently_occupied varchar(3),internet_provided varchar(3),last_electricity_unit int(5),agreement_ID varchar(20),agreement_Date date);")
-        cur.execute("Create table Payment_Details(payment_ID int(10),amount int(10),paid_by int(5));")
-    con.commit()
-    cur.close()
+
+        cur.execute("Create table Tenant_Details(Tenant_ID int(5) AUTO_INCREMENT primary key,Name varchar(25),address varchar(50),room_alloted varchar(10),last_balance int(10),extra_charge int(10),extra_charge_comment varchar(50),rent_paid_till date,left_room_on date,agreement_ID varchar(20));")
+
+        cur.execute("Create table Room_Details(room_ID int(5) AUTO_INCREMENT primary key,Room_name varchar(25),rent int(6),currently_occupied varchar(3),internet_provided varchar(3),last_electricity_unit int(5),agreement_ID varchar(20),agreement_Date date);")
+
+        cur.execute("Create table Payment_Details(payment_ID int(10) AUTO_INCREMENT primary key,amount int(10),paid_by int(5));")
+
+        add_room("first floor",5500,"YES","YES",1234,"alkdufdhlnadij","2023-03-01")
+        add_room("second floor room 1",3200,"YES","NO",1234,"alkdufdhlnadij","2023-03-01")
+        add_room("second floor room 2",3000,"NO","NO",1234,"alkdufdhlnadij","2023-03-01")
+        add_room("Shop no 5",5500,"YES","NO",1234,"alkdufdhlnadij","2023-03-01")
+        add_room("Shop no 6",5500,"YES","NO",1234,"alkdufdhlnadij","2023-03-01")
 initialise()
+def add_tenant(Name,address,room_alloted,last_balance,extra_charge,extra_charge_comment,rent_paid_till,left_room_on,agreement_ID):
+    cur.execute(f"insert into Tenant_Details(Name,address,room_alloted,last_balance,extra_charge,extra_charge_comment,rent_paid_till,left_room_on,agreement_ID) values('{Name}','{address}','{room_alloted}',{last_balance},{extra_charge},'{extra_charge_comment}',{rent_paid_till},'{left_room_on}','{agreement_ID}')")
+con.commit()
+cur.close()
