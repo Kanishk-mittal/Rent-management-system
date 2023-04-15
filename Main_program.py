feb08d61a37a4c037288fc49d4e9a1c9d2bdd5ee
Kanishk-mittal <kanishkmittal2005@gmail.com>
2023-04-15 09:37:19 +0530

    completed teh progam to display room details and started working on add tenanat

--- Main_program.py
+++ Main_program.py
@@ -9,5 +9,38 @@ sq.initialise()
 if g.ask_log_in():
     #here the user is authorised and we can perform our tasks
     room_data=sq.get_room_data()
+    print(room_data)
     choise=g.showrooms(room_data[["Room_name","currently_occupied","occupied_by"]])
-    print(g.get_function_value())
+    if int(g.get_function_value()) ==1:
+        if room_data.loc[1,"currently_occupied"]=="NO":
+            g.show_room_details("NULL","NULL","NULL")
+        else:
+            tenant_data=sq.get_tenant_details(room_data.loc[1,"occupied_by"])
+            g.show_room_details(str(tenant_data.Name),str(tenant_data.last_balance),str(tenant_data.rent_paid_till))
+    elif int(g.get_function_value()) ==2:
+        if room_data.loc[2,"currently_occupied"]=="NO":
+            g.show_room_details("NULL","NULL","NULL")
+        else:
+            tenant_data=sq.get_tenant_details(room_data.loc[1,"occupied_by"])
+            g.show_room_details(str(tenant_data.Name),str(tenant_data.last_balance),str(tenant_data.rent_paid_till))
+    elif int(g.get_function_value()) ==3:
+        if room_data.loc[3,"currently_occupied"]=="NO":
+            g.show_room_details("NULL","NULL","NULL")
+        else:
+            tenant_data=sq.get_tenant_details(room_data.loc[1,"occupied_by"])
+            g.show_room_details(str(tenant_data.Name),str(tenant_data.last_balance),str(tenant_data.rent_paid_till))
+    elif int(g.get_function_value()) ==4:
+        if room_data.loc[4,"currently_occupied"]=="NO":
+            g.show_room_details("NULL","NULL","NULL")
+        else:
+            tenant_data=sq.get_tenant_details(room_data.loc[1,"occupied_by"])
+            g.show_room_details(str(tenant_data.Name),str(tenant_data.last_balance),str(tenant_data.rent_paid_till))
+    elif int(g.get_function_value()) ==5:
+        if room_data.loc[3,"currently_occupied"]=="NO":
+            g.show_room_details("NULL","NULL","NULL")
+        else:
+            tenant_data=sq.get_tenant_details(room_data.loc[1,"occupied_by"])
+            g.show_room_details(str(tenant_data.Name),str(tenant_data.last_balance),str(tenant_data.rent_paid_till))
+            
+
+sq.close_connection()
--- SQL_connection.py
+++ SQL_connection.py
@@ -19,25 +19,32 @@ def initialise():
         cur.execute("Create Database Rent_details;")
         cur.execute("Use Rent_details;")
 
-        cur.execute("Create table Tenant_Details(Tenant_ID int(5) AUTO_INCREMENT primary key,Name varchar(25),address varchar(50),room_alloted varchar(10),last_balance int(10),extra_charge int(10),extra_charge_comment varchar(50),rent_paid_till date,left_room_on date,agreement_ID varchar(20));")
+        cur.execute("Create table Tenant_Details(Tenant_ID int(5) AUTO_INCREMENT primary key,Name varchar(25),address varchar(50),room_alloted varchar(10),last_balance int(10),rent_paid_till date,left_room_on date,agreement_ID varchar(20));")
 
         cur.execute("Create table Room_Details(room_ID int(5) AUTO_INCREMENT primary key,Room_name varchar(25),rent int(6),currently_occupied varchar(3),internet_provided varchar(3),last_electricity_unit int(5),agreement_ID varchar(20),agreement_Date date,occupied_by varchar(25));")
 
         cur.execute("Create table Payment_Details(payment_ID int(10) AUTO_INCREMENT primary key,amount int(10),paid_by int(5));")
 
-        add_room("first floor",5500,"YES","YES",1234,"alkdufdhlnadij","2023-03-01","NULL")
-        add_room("second floor room 1",3200,"YES","NO",1234,"alkdufdhlnadij","2023-03-01","NULL")
+        add_room("first floor",5500,"NO","YES",1234,"alkdufdhlnadij","2023-03-01","NULL")
+        add_room("second floor room 1",3200,"NO","NO",1234,"alkdufdhlnadij","2023-03-01","NULL")
         add_room("second floor room 2",3000,"NO","NO",1234,"alkdufdhlnadij","2023-03-01","NULL")
-        add_room("Shop no 5",5500,"YES","NO",1234,"alkdufdhlnadij","2023-03-01","NULL")
-        add_room("Shop no 6",5500,"YES","NO",1234,"alkdufdhlnadij","2023-03-01","NULL")
+        add_room("Shop no 5",5500,"NO","NO",1234,"alkdufdhlnadij","2023-03-01","NULL")
+        add_room("Shop no 6",5500,"NO","NO",1234,"alkdufdhlnadij","2023-03-01","NULL")
+
 def add_tenant(Name,address,room_alloted,last_balance,extra_charge,extra_charge_comment,rent_paid_till,left_room_on,agreement_ID):
     global cur
     cur.execute(f"insert into Tenant_Details(Name,address,room_alloted,last_balance,extra_charge,extra_charge_comment,rent_paid_till,left_room_on,agreement_ID) values('{Name}','{address}','{room_alloted}',{last_balance},{extra_charge},'{extra_charge_comment}',{rent_paid_till},'{left_room_on}','{agreement_ID}')")
+
 def get_room_data():
     global con
     # Read the table into a Pandas DataFrame
     df = pd.read_sql_query("SELECT * FROM  room_details", con,index_col="room_ID")
     return df
+
 def close_connection():
     con.commit()
     con.close()
+
+def get_tenant_details(name):
+    df = pd.read_sql_query(f"SELECT * FROM  tenant_details where name={name}", con,index_col="room_ID")
+    return df
